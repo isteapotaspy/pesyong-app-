3e288e316fa1bdcb43ea10d0cf6ba0bd78c91bc9
Jane <jennmiku@gmail.com>
2026-03-10 00:17:11 +0800

    feat: add BooleanToVisibilityConverter

---
+++ PESYONG.Application/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace PESYONG.ApplicationLogic.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value is bool boolValue && boolValue
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility visibility && visibility == Visibility.Visible;
+        }
+    }
+}
