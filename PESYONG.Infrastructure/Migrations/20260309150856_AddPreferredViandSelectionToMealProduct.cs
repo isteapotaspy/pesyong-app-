34fece5b3d01d7b9ad63057ba7c5d4159cce7f7a
Jane <jennmiku@gmail.com>
2026-03-10 00:17:30 +0800

    feat: add Migrations

---
+++ PESYONG.Infrastructure/Migrations/20260309150856_AddPreferredViandSelectionToMealProduct.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PESYONG.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreferredViandSelectionToMealProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowPreferredViands",
+                table: "MealProducts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PreferredViandCount",
+                table: "MealProducts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowPreferredViands",
+                table: "MealProducts");
+
+            migrationBuilder.DropColumn(
+                name: "PreferredViandCount",
+                table: "MealProducts");
+        }
+    }
+}
